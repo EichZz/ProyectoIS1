2e556081c80c32547bc47d189fe7fa6bf9b00216
EichZz <hectoramm.rubio@gmail.com>
2018-11-23 13:09:14 +0100

    Parte de clase alumno

---
+++ ProyectoIS1/src/Aplicacion/Alumno.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplicacion;
+import java.util.*;
+/**
+ *
+ * @author hecto
+ */
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private int grupo_de_EPD;
+    private List listaNotas;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+    
+}
