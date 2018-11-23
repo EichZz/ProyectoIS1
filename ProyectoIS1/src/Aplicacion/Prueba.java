fb688d5a1946c2ad42aaa68ddc57ff1a1d7cca5c
EichZz <hectoramm.rubio@gmail.com>
2018-11-23 12:38:05 +0100

    setter

--- ProyectoIS1/src/Aplicacion/Prueba.java
+++ ProyectoIS1/src/Aplicacion/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
     
     private int id_Prueba;
 
-    public int getId_Prueba() {
-        return id_Prueba;
+    public void setId_Prueba(int id_Prueba) {
+        this.id_Prueba = id_Prueba;
     }
 }
