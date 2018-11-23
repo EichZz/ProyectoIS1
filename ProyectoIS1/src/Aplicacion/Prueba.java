32ec8328099811e090671279214e12e643916206
EichZz <hectoramm.rubio@gmail.com>
2018-11-23 12:41:12 +0100

    getter y setter

--- ProyectoIS1/src/Aplicacion/Prueba.java
+++ ProyectoIS1/src/Aplicacion/Prueba.java
@@ -5,6 +5,10 @@ public class Prueba {
     
     private int id_Prueba;
 
+    public int getId_Prueba() {
+        return id_Prueba;
+    }
+
     public void setId_Prueba(int id_Prueba) {
         this.id_Prueba = id_Prueba;
     }
