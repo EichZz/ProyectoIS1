1a43d78af3c94e5bf66111bce3335e764ae3aecc
Ivan <ivanmoralesse@gmail.com>
2018-11-23 12:59:04 +0100

    nuevo

--- ProyectoIS1/src/Aplicacion/Nota.java
+++ ProyectoIS1/src/Aplicacion/Nota.java
@@ -5,5 +5,20 @@ public class Nota {
     private Prueba prueba;
     private double calificacion;
     
-    
+    public void addNota(Nota n){
+        
+    }
+
+    public void setPrueba(Prueba prueba) {
+        this.prueba = prueba;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Nota{" + "prueba=" + prueba + ", calificacion=" + calificacion + '}';
+    }
 }
