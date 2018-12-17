0ec42fc3b015f9ce795306c96b49fbc698c3ea69
EichZz <hectoramm.rubio@gmail.com>
2018-12-17 12:35:54 +0100

    Todo corregido y comentado

--- ProyectoIS1/src/Aplicacion/main.java
+++ ProyectoIS1/src/Aplicacion/main.java
@@ -9,6 +9,20 @@ public class main {
     public static void main(String[] args) {
         Pantalla pantalla = new Pantalla();        
         pantalla.MenuPrincipal();
-    }//
+    }
+    /* Datos introducidos:
+            Alumno a1 = new Alumno("Ana", "Perez Gomez", "34895017F", 1);
+            Alumno a2 = new Alumno("Paco", "Gonzalez Burgos", "60139345Y", 1);
+            Alumno a3 = new Alumno("Maria", "Fernandez Carmona", "73904457S", 2);
+    
+    
+            Prueba epd1 = new Prueba(1, "EPD Eval 1", "Primer parcial", 20.0, "Evaluacion continua");
+            Prueba epd2 = new Prueba(2, "EPD Eval 2", "Segundo parcial", 30.0, "Evaluacion continua");
+            Prueba conv = new Prueba(3, "Convocatoria", "Examen de enero", 50.0, "Evaluacion final");
+           
+            Grupo g = new Grupo();
+            g.setId_Grupo(1);
+    */
+    
     
 }
